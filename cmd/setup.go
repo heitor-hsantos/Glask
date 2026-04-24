065312227cf6b0d98ccb70d7d2a8054356159e26
heitor-hsantos <heitor.santos118@gmail.com>
2026-04-24 12:19:11 -0300

    feat: add Cobra command for Gemini API key configuration

--- cmd/setup.go
+++ cmd/setup.go
@@ -1,1 +1,12 @@
 package cmd
+
+import (
+	"github.com/spf13/cobra"
+)
+
+var ApiCfg = &cobra.Command{
+	Use:   "api_key_gmn",
+	Short: "Gemini Api Key config command",
+	Long:  "Api Key config command it insert a new api Key for Gemini",
+	Run:   func(cmd *cobra.Command, args []string) {},
+}
